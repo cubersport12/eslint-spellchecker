97bd75a3f9d8334b7c4f1ab9ae7fb8d2b1110a09
ivanovaa <ivanovaa@mir-omsk.ru>
2023-11-14 10:14:27 +0600

    fixes

--- libs/ts-spellchecker/src/lib/ts-spellchecker.ts
+++ libs/ts-spellchecker/src/lib/ts-spellchecker.ts
@@ -1,9 +1,7 @@
-import { ESLintUtils } from '@typescript-eslint/utils'
 const Spellchecker = require('hunspell-spellchecker');
 import { dirname, join, relative } from 'path';
 import { readFileSync } from 'fs';
-
-const createRule = ESLintUtils.RuleCreator(name => 'ts-spellchecker');
+import { Rule } from 'eslint';
 
 type OptionsType = {
   matchWordRegex: string;
@@ -27,62 +25,69 @@ const loadHunspellDic = (options: OptionsType) => {
   spellchecker.use(dict);
   return spellchecker;
 };
+
+
+const meta: Rule.RuleMetaData = {
+  docs: {
+    description: 'Spellchecker',
+    recommended: true
+  },
+  schema: [
+    {
+      type: 'object',
+      properties: {
+        matchWordRegex: { type: 'string', default: '[А-Яа-я]+' },
+        dicFolder: { type: 'string', default: './dics/ru' },
+        dicName: { type: 'string', default: 'ru_RU' }
+      }
+    }
+  ],
+  messages: {
+    misspelled: 'Нет слова в словаре "{{word}}"'
+  },
+  type: 'problem',
+  hasSuggestions: true
+}
+
 let dic: any;
 const wordsCache = new Set<string>();
 
 
-export const TsSpellchekerRule = createRule({
-  create(context) {
-    const options: OptionsType = context.options[0];
-    if (!options.dicFolder) {
-      throw new Error('dicPath not found');
-    }
-    if (!dic) {
-      dic = loadHunspellDic(options);
-    }
-    return {
-      Literal(node) {
-        const regex = new RegExp(options.matchWordRegex, 'g');
-        const result = `${node.value}`.match(regex)?.filter((x) => Boolean(x));
-        if (!result || result.length === 0) {
+const create = (context: Rule.RuleContext) => {
+  const options: OptionsType = context.options[0];
+  if (!options.dicFolder) {
+    throw new Error('dicPath not found');
+  }
+  if (!dic) {
+    dic = loadHunspellDic(options);
+  }
+  return {
+    'Literal'(node) {
+      const regex = new RegExp(options.matchWordRegex, 'g');
+      const result = `${node.value}`.match(regex)?.filter((x) => Boolean(x));
+      if (!result || result.length === 0) {
+        return;
+      }
+      result.map(x => x.toLowerCase()).forEach((r) => {
+        if (wordsCache.has(r)) {
           return;
         }
-        result.map(x => x.toLowerCase()).forEach((r) => {
-          if (wordsCache.has(r)) {
-            return;
-          }
-          wordsCache.add(r);
-          if (!dic.checkExact(r)) {
-            context.report({
-              node: node as any,
-              messageId: 'misspelled'
-            });
-          }
-        });
-      }
-    }
-  },
-  name: 'ts-spellchecker',
-  meta: {
-    docs: {
-      description: 'Spellchecker',
-      recommended: 'recommended'
-    },
-    schema: [
-      {
-        type: 'object',
-        properties: {
-          matchWordRegex: { type: 'string', default: '[А-Яа-я]+' },
-          dicFolder: { type: 'string', default: './dics/ru' },
-          dicName: { type: 'string', default: 'ru_RU' }
+        wordsCache.add(r);
+        if (!dic.checkExact(r)) {
+          context.report({
+            node: node as any,
+            messageId: 'misspelled',
+            data: {
+              word: r
+            }
+          });
         }
-      }
-    ],
-    messages: {
-      misspelled: 'Нет слова в словаре "{{word}}"'
-    },
-    type: 'problem',
-    hasSuggestions: true
-  },
-  defaultOptions: [],
-})
+      });
+    }
+  }
+}
+
+export const TsSpellchekerRule: Rule.RuleModule = {
+  create,
+  meta
+}
